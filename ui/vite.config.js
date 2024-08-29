2984a18b2115f13e4b02bba1c94740db6a463b00
Bhavesh Kakadiya <bhaveshkakadiya24@gmail.com>
2024-08-29 17:47:42 -0400

    Set development

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === 'development';
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: isDevelopment
+        ? {
+            '/api': 'http://localhost:3000',
+          }
+        : {},
+    },
+  };
+});
